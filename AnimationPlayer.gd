161b88e429b406d609da94f5f52edc71339a6f18
Jimmy-Jazz-x <mathew.p.marshall@gmail.com>
2023-11-14 20:45:34 -0400

    Added Sprite

---
+++ AnimationPlayer.gd
@@ -0,0 +1,14 @@
+extends AnimationPlayer
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+	play("Idle")
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func  _on_animation_finished(anim_name):
+	play("Idle")
